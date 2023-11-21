48a6534c3d0f5cd717b424864a9c50e833a67776
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-11-21 16:38:12 +0100

    restructure

--- route/index.js
+++ route/index.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const { Readable, Transform } = require('node:stream')
 const csvParser = require('csv-parser');
 const streamDataEntry = require('../model')
-
+const {pipeline} = require('stream')
 
 // Set up Multer for file upload
 const storage = multer.memoryStorage();
@@ -38,80 +38,40 @@ router.post('/upload', upload.single('file'), async (req, res) => {
   
   // Function to process CSV file
   async function FileuploadProcess(buffer) {
-    const entries = [];
 
-    // Use a pipeline to simplify the stream setup
-    await new Promise((resolve, reject) => {
-      pipeline(
-        // Readable stream
-        Readable.from(buffer),
+    const readStream = new Readable();
+    readStream.push(buffer);
+    readStream.push(null); // Signal the end of the stream
   
-        // csv-parser
-        csvParser({
-          mapHeaders: ({ header, index }) => header.trim(),
-        }),
+    const entries = [];
   
-        // Custom Transform stream
-        new Transform({
-          objectMode: true,
-          transform(chunk, encoding, callback) {
-            const MSISDN = normalizeMSISDN(chunk.MSISDN);
-            console.time()
-            this.push({
-              MSISDN,
-              quantity: parseInt(chunk.quantity),
-              narration: chunk.narration,
-            });
-            console.time()
-            callback();
-          },
-        }),
-  
-        // For async iteration and parallel processing
-        async function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
+    const csvTransform = csvParser({
+      mapHeaders: ({ header, index }) => header.trim(),
     });
   
+    const transformStream = new Transform({
+      objectMode: true,
+      transform(chunk, encoding, callback) {
+        const MSISDN = normalizeMSISDN(chunk.MSISDN);
+  
+        this.push({
+          MSISDN,
+          quantity: parseInt(chunk.quantity),
+          narration: chunk.narration,
+        });
+        callback();
+      },
+    });
+  
+    // Pipe the ReadStream through csv-parser and the custom transform stream
+    readStream.pipe(csvTransform).pipe(transformStream);
+  
+    // Collect the transformed entries
+    for await (const entry of transformStream) {
+      entries.push(entry);
+    }
+  
     return entries;
-    // const readStream = new Readable();
-    // readStream.push(buffer);
-    // readStream.push(null); // Signal the end of the stream
-  
-    // const entries = [];
-  
-    // const csvTransform = csvParser({
-    //   mapHeaders: ({ header, index }) => header.trim(),
-    // });
-  
-    // const transformStream = new Transform({
-    //   objectMode: true,
-    //   transform(chunk, encoding, callback) {
-    //     const MSISDN = normalizeMSISDN(chunk.MSISDN);
-  
-    //     this.push({
-    //       MSISDN,
-    //       quantity: parseInt(chunk.quantity),
-    //       narration: chunk.narration,
-    //     });
-    //     callback();
-    //   },
-    // });
-  
-    // // Pipe the ReadStream through csv-parser and the custom transform stream
-    // readStream.pipe(csvTransform).pipe(transformStream);
-  
-    // // Collect the transformed entries
-    // for await (const entry of transformStream) {
-    //   entries.push(entry);
-    // }
-  
-    // return entries;
   }
   
 
